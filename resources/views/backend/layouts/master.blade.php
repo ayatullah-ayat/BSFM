6a3298c3808dec94038d5e91d014b00bfe1d54fb
Alauddin Jahin <alauddinjahin24@gmail.com>
2022-03-03 14:04:43 +0600

    nothing change

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -8,14 +8,14 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
     <title>@yield('title','Micro Midea')</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('assets/common_assets/libs/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
 
     <!-- Daterange picker -->
     <link rel="stylesheet" href="{{ asset('assets/common_assets/libs/daterangepicker/daterangepicker.css') }}">
