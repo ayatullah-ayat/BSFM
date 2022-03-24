ea82b4c4916e5c4c20155b328205c6cf3bd6f050
Alauddin Jahin <alauddinjahin24@gmail.com>
2022-03-24 11:10:11 +0600

    added office migration

---
+++ app/Http/Controllers/Admin/OfficeAccountController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Models\OfficeAccount;
+use App\Http\Controllers\Controller;
+
+class OfficeAccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OfficeAccount  $officeAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OfficeAccount $officeAccount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OfficeAccount  $officeAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OfficeAccount $officeAccount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OfficeAccount  $officeAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OfficeAccount $officeAccount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OfficeAccount  $officeAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OfficeAccount $officeAccount)
+    {
+        //
+    }
+}
