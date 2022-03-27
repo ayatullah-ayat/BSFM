a1c1d331f92c25a03596237c8bd80d4630723703
mamun060 <obujraju420@gmail.com>
2022-03-27 09:52:35 +0600

    27th march morning commit

--- app/Http/Controllers/Admin/PurchaseController.php
+++ app/Http/Controllers/Admin/PurchaseController.php
@@ -20,7 +20,7 @@ class PurchaseController extends Controller
      */
     public function index()
     {
-        // 
+        return view('backend.pages.purchase.managepurchase');
     }
 
     /**
